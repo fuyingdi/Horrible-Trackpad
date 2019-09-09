c00ff5e1766c0c67941bee9b93bbfe6fc37428ea
fuyingdi <fuyingdi@126.com>
2019-09-09 09:26:58 +0800

    use oo

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from tkinter import *
 
+class Root(Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("233x286")
+        self.resizable(False,False)
+
+        self.canvas = Canvas(self, width=233, height=286, bg='black')
+        self.num_pad_image = PhotoImage(file="number_pad.png")
+        self.canvas.create_image(0,0, image=self.num_pad_image, anchor=NW)
+        self.canvas.pack()
+
+    def init_number_pad(self):
+        number_pad_slot = [[]]
+        number_pad_slot[0].append(Label(self,textvariable="o"))
+        for x in number_pad_slot:
+            for y in x:
+                y.pack()
+
+def run():
+    root = Root()
+    root.init_number_pad()
+    
+    root.mainloop()
+
+
 
 if __name__ == '__main__':
-    root = Tk()
-    root.geometry("233x286")
-    root.resizable(False,False)
-    canvas = Canvas( width=300, height=200, bg='black')
-    canvas.pack(expand=YES, fill=BOTH)
-    image = PhotoImage(file="number_pad.png")
-    canvas.create_image(0, 0, image=image, anchor=NW)
-    root.mainloop()
+    run()
